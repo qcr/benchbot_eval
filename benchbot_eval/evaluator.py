5e50658d6bb142d06926e4da9e5d84d3ad4b4064
ben <b.talbot@qut.edu.au>
2021-01-27 17:41:35 +1000

    Load & call evaluate function from addons

--- benchbot_eval/evaluator.py
+++ benchbot_eval/evaluator.py
@@ -11,6 +11,12 @@ class Evaluator:
             "evaluation_methods", [("name", evaluation_method)],
             return_data=True)
 
+        self.evaluate_fns = bam.load_functions(self.evaluation_method_data)
+        assert 'evaluate' in self.evaluate_fns.keys(), (
+            "No 'evaluate' function was found when loading method '%s'",
+            evaluation_method)
+        self.evaluate_fn = self.evaluate_fns['evaluate']
+
         self.results_data = None
 
         self.required_task = None
@@ -31,18 +37,31 @@ class Evaluator:
         if not valid_results:
             print("Exiting, as no valid results were provided.")
 
+        # Load available ground truth data
+        # TODO ideally this would only load what is required, but the manager
+        # in benchbot_addons would need to support nested 'get_match' queries
+        gt_data = bam.load_yaml_list(bam.find_all("ground_truths", 'json'))
+
         # Iterate through results, attempting evaluation
-        supported_formats = self.evaluation_method_data[
-            'valid_ground_truth_formats']
+        gt_formats = self.evaluation_method_data['valid_ground_truth_formats']
+        scores_data = []
         for k, v in self.results_data.items():
             # Find a usable ground truth (one of supported formats, & for same
             # environment as this result
-            gt = next((g for g in self.ground_truth_data
-                       if helpers.env_string(g['environment_details']) ==
-                       helpers.env_string(v['environment_details']) and g['']),
-                      None)
+            gts = []
+            for e in v['environment_details']:
+                gt = next(
+                    (g for g in gt_data
+                     if bam.env_string(g['environment']) == bam.env_string(e)
+                     and g['format']['name'] in gt_formats), None)
+                assert gt is not None, (
+                    "No ground truth could was found for '%s' with "
+                    "any of the following formats:\n\t%s" %
+                    (bam.env_string(e), gt_formats))
+                gts.append(gt)
 
             # Score the result
+            scores_data.append(self.evaluate_fn(v, gts))
 
             # Print the results (if allowed)
 
