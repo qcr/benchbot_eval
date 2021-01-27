539265c2098115e77cfd36439bec4b3accb4868d
ben <b.talbot@qut.edu.au>
2021-01-27 11:32:09 +1000

    Update setup.py metadata

--- setup.py
+++ setup.py
@@ -3,25 +3,24 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(
-    name='benchbot_eval',
-    version='0.1.3',
-    author='Ben Talbot',
-    author_email='b.talbot@qut.edu.au',
-    description=
-    'The BenchBot evaluation suite for use with the ACRV Scene Understanding Challenge',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    packages=find_packages(),
-    install_requires=['numpy', 'pickle', 'shapely'],
-    classifiers=(
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-    ))
+setup(name='benchbot_eval',
+      version='2.0.0',
+      author='Ben Talbot',
+      author_email='b.talbot@qut.edu.au',
+      description=
+      'The BenchBot evaluation tools for use with the BenchBot software stack',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=find_packages(),
+      install_requires=['numpy', 'pickle', 'shapely'],
+      classifiers=(
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
+      ))
